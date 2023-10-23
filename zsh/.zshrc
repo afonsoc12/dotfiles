c05f47217834899c5e76cbf9394076c058732732
Afonso Costa <afonsoc1997@gmail.com>
2023-10-23 23:38:23 +0100

    ZSH dotfiles

---
+++ zsh/.zshrc
@@ -0,0 +1,21 @@
+# .zshrc
+# Sourced in interactive shells.
+# Should contain commands to set up aliases, functions, options, key bindings, etc.
+
+# Path to your oh-my-zsh installation.
+echo "On .zshrc"
+
+# oh-my-zsh
+export ZSH=$XDG_DATA_HOME/oh-my-zsh
+
+ZSH_THEME="random"
+# ZSH_THEME_RANDOM_CANDIDATES=( "robbyrussell" "agnoster" "fino-time")
+COMPLETION_WAITING_DOTS="true"
+
+zstyle ':omz:update' frequency 15
+
+plugins=(git)
+
+source $ZSH/oh-my-zsh.sh
+
+source $XDG_CONFIG_HOME/zsh/aliases.zsh
---
+++ zsh/aliases.zsh
@@ -0,0 +1,18 @@
+# General
+alias cdd="cd ~/Desktop"
+alias cdh="cd ~"
+alias v="ls -alh"
+alias dotfiles="/usr/bin/git --git-dir=$XDG_CONFIG_HOME/dotfiles --work-tree=$XDG_CONFIG_HOME"
+
+# Kubernetes aliases
+alias k="kubectl"
+export do="--dry-run=client -oyaml"
+export now="--grace-period=0 --force"
+alias kn="kubectl config set-context --current --namespace"
+alias ka="kubectl apply -f"
+alias kc="kubectl create"
+alias kr="kubectl run"
+alias kg="kubectl get"
+alias kd="kubectl describe"
+alias kdebug="kubectl run --rm -i --restart=Never"
+alias kubeseal="kubeseal --controller-name=sealed-secrets --controller-namespace=utils"
